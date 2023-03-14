456ee9c1a4c681fbfa122e86689a0dbd8fa66adf
talib-tambe <60101158@udst.edu.qa>
2023-03-14 19:03:34 +0300

    add divide function

--- test_calculator.py
+++ test_calculator.py
@@ -12,4 +12,7 @@ class TestCalculator:
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
 
+    def test_division(self):
+        assert 100 == calculator.division(1000, 10)
+
     
